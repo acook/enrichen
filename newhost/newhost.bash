24a0d67c3db5397e0aa31aa4da882d8149299bbb
Anthony Cook <github@anthonymcook.com>
2024-01-18 20:16:04 -0600

    Ensure each command gets the host properly

--- newhost/newhost.bash
+++ newhost/newhost.bash
@@ -11,10 +11,21 @@ echo ${1:-WHAT}
 remote_host="$1"
 setup_script="newhost_setup.bash"
 
-if ! [[ $remote_host == *:* ]]; then
+if [[ $remote_host == *:* ]]; then
+    ssh_remote_arry=(${remote_host//:/ })
+    ssh_remote_host="${ssh_remote_arry[0]}"
+    ssh_remote_path="${ssh_remote_arry[1]}"
+
+    scp_remote_host="$remote_host"
+else
+    ssh_remote_host="$remote_host"
+    ssh_remote_path=""
+
+    # scp will happily do nothing at all if there is no colon with no error message
     scp_remote_host="$remote_host:"
 fi
 
 run_or_die "script copy to remote host" scp $setup_script $scp_remote_host
 
-run_or_die "script on remote host" ssh $remote_host "bash $setup_script"
+run_or_die "script on remote host" ssh "$ssh_remote_host" "bash $ssh_remote_path/$setup_script"
+
