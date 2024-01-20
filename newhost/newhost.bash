ecd7fa385ef2546cdab5f73dc9e67db7d8d80a35
Anthony Cook <github@anthonymcook.com>
2024-01-19 20:31:30 -0600

    Automatically find enrichments directory

--- newhost/newhost.bash
+++ newhost/newhost.bash
@@ -6,10 +6,12 @@ if ! command_exists scp; then
     die "command 'scp' required to load the install script onto the remote host"
 fi
 
-echo ${1:-WHAT}
-
 remote_host="$1"
+enrichments_dir="$MODERN_MAIN_DIR/enrichments"
 setup_script="newhost_setup.bash"
+
+say "detected current directory as: '$(thisdir)'"
+say "detected enrichment directory as: '$enrichments_dir'"
 
 if [[ $remote_host == *:* ]]; then
     ssh_remote_arry=(${remote_host//:/ })
@@ -25,7 +27,7 @@ else
     scp_remote_host="$remote_host:"
 fi
 
-run_or_die "script copy to remote host" scp $setup_script $scp_remote_host
+run_or_die "script copy to remote host" scp "$enrichments_dir/$setup_script" "$scp_remote_host"
 
 run_or_die "script on remote host" ssh "$ssh_remote_host" "bash $ssh_remote_path/$setup_script"
 
