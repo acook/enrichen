e274ff68eab6d6594cfdacec5c0d956628ad549a
Anthony Cook <github@anthonymcook.com>
2024-01-29 11:45:37 -0600

    Switch from scp to modern.sh's txcp

--- newhost/newhost.bash
+++ newhost/newhost.bash
@@ -2,10 +2,6 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/lib/modern.sh"
 
-if ! command_exists scp; then
-    die "command 'scp' required to load the install script onto the remote host"
-fi
-
 remote_host="$1"
 enrichments_dir="$MODERN_MAIN_DIR/enrichments"
 setup_script="newhost_setup.bash"
@@ -27,7 +23,7 @@ else
     scp_remote_host="$remote_host:"
 fi
 
-run_or_die "script copy to remote host" scp "$enrichments_dir/$setup_script" "$scp_remote_host"
+run_or_die "script copy to remote host" txcp "$ssh_remote_host" "$enrichments_dir/$setup_script" "$ssh_remote_path/$setup_script"
 
 run_or_die "script on remote host" ssh "$ssh_remote_host" "bash $ssh_remote_path/$setup_script"
 
