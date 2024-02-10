8d91e2bf316abf639a52f6e4e84ba548e238f7b5
Anthony Cook <github@anthonymcook.com>
2024-02-10 05:45:03 -0600

    Remove dead and inefficient code

--- lib/enrichen_remote.bash
+++ lib/enrichen_remote.bash
@@ -14,15 +14,9 @@ say "detected enrichment directory as: '$enrichments_dir'"
 if [[ $remote_host == *:* ]]; then
   ssh_remote_arry=(${remote_host//:/ })
   ssh_remote_host="${ssh_remote_arry[0]}"
-  ssh_remote_path="${ssh_remote_arry[1]}"
 else
   ssh_remote_host="$remote_host"
-  ssh_remote_path=""
 fi
-
-run_or_die "script copy to remote host" txcp "$ssh_remote_host" "$enrichments_dir/$setup_script" "$ssh_remote_path/$setup_script"
-
-run_or_die "script on remote host" ssh "$ssh_remote_host" "bash $ssh_remote_path/$setup_script"
 
 say "streaming commands to remote host"
 
@@ -31,7 +25,7 @@ safe_cd "$MODERN_MAIN_DIR/../tmp"
 
 sshpipe_new "$ssh_remote_host"
 
-cat "$MODERN_MAIN_DIR/../lib/remote_prompt.bash" "$MODERN_SCRIPT_FULLPATH" "$enrichments_dir/$enrichments" >&13
+cat "$MODERN_MAIN_DIR/../lib/remote_prompt.bash" "$MODERN_SCRIPT_FULLPATH" "$enrichments_dir/$enrichments" "$enrichments_dir/$setup_script" >&13
 sshpipe_rx "$ssh_remote_host"
 sleep 5
 
