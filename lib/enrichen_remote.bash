f046eb37bec10a5d0d801ef2e942545bda2f92b9
Anthony Cook <github@anthonymcook.com>
2024-02-10 04:15:24 -0600

    Remove dead code

--- lib/enrichen_remote.bash
+++ lib/enrichen_remote.bash
@@ -12,28 +12,19 @@ say "detected current directory as: '$(thisdir)'"
 say "detected enrichment directory as: '$enrichments_dir'"
 
 if [[ $remote_host == *:* ]]; then
-    ssh_remote_arry=(${remote_host//:/ })
-    ssh_remote_host="${ssh_remote_arry[0]}"
-    ssh_remote_path="${ssh_remote_arry[1]}"
-
-    scp_remote_host="$remote_host"
+  ssh_remote_arry=(${remote_host//:/ })
+  ssh_remote_host="${ssh_remote_arry[0]}"
+  ssh_remote_path="${ssh_remote_arry[1]}"
 else
-    ssh_remote_host="$remote_host"
-    ssh_remote_path=""
-
-    # scp will happily do nothing at all if there is no colon with no error message
-    scp_remote_host="$remote_host:"
+  ssh_remote_host="$remote_host"
+  ssh_remote_path=""
 fi
 
 run_or_die "script copy to remote host" txcp "$ssh_remote_host" "$enrichments_dir/$setup_script" "$ssh_remote_path/$setup_script"
 
 run_or_die "script on remote host" ssh "$ssh_remote_host" "bash $ssh_remote_path/$setup_script"
 
-#run_or_die "loading enrichments on remote host" txcp "$ssh_remote_host" "$enrichments_dir/$enrichments" "$ssh_remote_path/$setup_script"
-
 say "streaming commands to remote host"
-
-#cat "$MODERN_SCRIPT_FULLPATH" "$enrichments_dir/$enrichments" | ssh "$ssh_remote_host" "bash -s"
 
 mkdir -v -p "$MODERN_MAIN_DIR/../tmp"
 safe_cd "$MODERN_MAIN_DIR/../tmp"
