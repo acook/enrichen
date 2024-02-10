2aa88befa6f6d218bffa3e8238acecec0575a287
Anthony Cook <github@anthonymcook.com>
2024-02-10 03:18:51 -0600

    Update script knows where modern.sh is

--- lib/update_enrichen_deps.bash
+++ lib/update_enrichen_deps.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source "$(dirname "${BASH_SOURCE[0]}")/lib/modern.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/modern.sh"
 
 safe_cd "$MODERN_SCRIPT_DIR"
 run "downloading latest modern.sh" curl -O -L https://raw.githubusercontent.com/acook/modern.sh/main/modern.sh
